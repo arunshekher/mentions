fbb8db0758c7dccdb93b0afdee0d3302a9adf96a
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-05 02:20:00 +0400

    Code cleanup.

--- MentionsSuggestion.php
+++ MentionsSuggestion.php
@@ -1,52 +1,28 @@
 <?php
-//require __DIR__ . '/Mentions.php';
+
 
 class MentionsSuggestion extends Mentions
 {
 
 
 	/**
-	 * Load suggestions popup javascript libs.
+	 * Trigger response for the query
+	 *
+	 * @param $request
 	 */
-	public function loadLibs()
+	public static function triggerResponse($request)
 	{
-		// plugin preferences - shared
-		$mentionsPref = $this->prefs;
+		$suggestion = new MentionsSuggestion;
+		$suggestion->respond($request);
 
-		if ($mentionsPref['mentions_active'] && USER_AREA && USER) {
-
-			$libGlobal = $mentionsPref['use_global_path'];
-
-
-			if ($libGlobal) {
-				e107::library('load', 'ichord.caret');
-				e107::library('load', 'ichord.atwho');
-			} else {
-				e107::css('mentions', 'js/ichord.atwho/dist/css/jquery.atwho.min.css');
-				e107::js('footer', e_PLUGIN . 'mentions/js/ichord.caret/dist/jquery.caret.min.js', 'jquery', 1);
-				e107::js('footer', e_PLUGIN . 'mentions/js/ichord.atwho/dist/js/jquery.atwho.min.js', 'jquery', 2);
-			}
-
-			// Mentions Autocomplete/suggestion API path
-			$apiPath = e_PLUGIN_ABS . 'mentions/index.php';
-
-			$jsSettings = [
-				'api_endpoint'  => $apiPath,
-				'suggestions'    => [
-					'minChar' => $mentionsPref['atwho_min_char'],
-					'maxChar' => $mentionsPref['atwho_max_char'],
-					'entryLimit' => $mentionsPref['atwho_item_limit']
-				]
-			];
-
-			// Footer - settings + script
-			e107::js('settings', ['mentions' => $jsSettings]);
-			e107::js('footer', '{e_PLUGIN}mentions/js/mentions.js', 'jquery');
-		}
 	}
 
+
 	/**
-	 * Responds to suggestions API requests, returns JSON formatted response.
+	 * Responds to suggestions API requests,
+	 * returns JSON formatted response.
+	 *
+	 * @param $request
 	 */
 	public function respond($request)
 	{
@@ -56,8 +32,7 @@ class MentionsSuggestion extends Mentions
 			$db = e107::getDb();
 			$tp = e107::getParser();
 
-			$mq =
-				$tp->filter($request); // TODO --> ? Shouldn't it be mysql escaping (->toDB) - but will it cause server overhead??
+			$mq = $tp->filter($request);
 			$where = "user_name LIKE '" . $mq . "%' ";
 
 			if ($db->select('user', 'user_name, user_image, user_login',
@@ -67,14 +42,13 @@ class MentionsSuggestion extends Mentions
 				$data = [];
 				while ($row = $db->fetch()) {
 					$data[] = [
-						'image' => $row['user_image'],
+						'image'    => $row['user_image'],
 						'username' => $row['user_name'],
 						'name'     => $row['user_login'],
 					];
 				}
 
 				if (count($data)) {
-					//TODO -  ?? $data need filter_var_array as its going to be parsed as html on frontend, ?? xss csrf
 					$ajax = e107::getAjax();
 					$ajax->response($data);
 				}
@@ -82,9 +56,9 @@ class MentionsSuggestion extends Mentions
 
 				$msg = [
 					'error' => [
-						'msg' => 'No user found!',
+						'msg'  => 'No user found!',
 						'code' => '4',
-					]
+					],
 				];
 
 				e107::getAjax()->response($msg);
@@ -97,21 +71,57 @@ class MentionsSuggestion extends Mentions
 
 
 	/**
-	 * Trigger response for the query
-	 * @param $request
+	 * Load libs
 	 */
-	public static function triggerResponse($request)
-	{
-		$suggestion = new MentionsSuggestion;
-		$suggestion->respond($request);
-
-	}
-
-
 	public static function libs()
 	{
 		$suggestion = new MentionsSuggestion;
 		$suggestion->loadLibs();
+	}
+
+
+	/**
+	 * Load suggestions popup javascript libs.
+	 */
+	public function loadLibs()
+	{
+		// plugin preferences
+		$mentionsPref = $this->prefs;
+
+		if ($mentionsPref['mentions_active'] && USER_AREA && USER) {
+
+			$libGlobal = $mentionsPref['use_global_path'];
+
+			if ($libGlobal) {
+				e107::library('load', 'ichord.caret');
+				e107::library('load', 'ichord.atwho');
+			} else {
+				e107::css('mentions',
+					'js/ichord.atwho/dist/css/jquery.atwho.min.css');
+				e107::js('footer',
+					e_PLUGIN . 'mentions/js/ichord.caret/dist/jquery.caret.min.js',
+					'jquery', 1);
+				e107::js('footer',
+					e_PLUGIN . 'mentions/js/ichord.atwho/dist/js/jquery.atwho.min.js',
+					'jquery', 2);
+			}
+
+			// Mentions Autocomplete/suggestion API path
+			$apiPath = e_PLUGIN_ABS . 'mentions/index.php';
+
+			$jsSettings = [
+				'api_endpoint' => $apiPath,
+				'suggestions'  => [
+					'minChar'    => $mentionsPref['atwho_min_char'],
+					'maxChar'    => $mentionsPref['atwho_max_char'],
+					'entryLimit' => $mentionsPref['atwho_item_limit'],
+				],
+			];
+
+			// Footer - settings + script
+			e107::js('settings', ['mentions' => $jsSettings]);
+			e107::js('footer', '{e_PLUGIN}mentions/js/mentions.js', 'jquery');
+		}
 	}
 
 }
