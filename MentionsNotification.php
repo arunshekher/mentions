97d70b01c089b07c7f86120e7c65aaa3321bb99f
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-04 20:37:04 +0400

    Rough draft for notification feature. Only chatbox post implemented at this time. Can be optimized a lot for speed.

--- MentionsNotification.php
+++ MentionsNotification.php
@@ -3,5 +3,190 @@
 
 class MentionsNotification extends Mentions
 {
+	protected $mentions;
+	protected $message;
+	protected $content;
+
+	protected $mentioner;
+	protected $mentioneeData;
+
+
+	/**
+	 * Listen to and perform notification
+	 */
+	public function perform()
+	{
+		if (USER && (strtolower($_SERVER['REQUEST_METHOD']) === 'post' || e_AJAX_REQUEST)) {
+			// chatbox
+			if ($_POST['chat_submit'] && $_POST['cmessage'] !== '') {
+				// Debug
+				$this->log(json_encode([USERNAME, $_POST['cmessage']]));
+				$mentions = $this->getAllMentions($_POST['cmessage']);
+				if ($mentions) {
+					$this->message = $_POST['cmessage'];
+					$this->mentions = $mentions;
+					$this->mentioner = USERNAME;
+					$this->notifyAllMentioned();
+					// todo: unset mentions and message
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Debug log method
+	 * @param string $content
+	 * @param string $logname
+	 */
+	public function log($content, $logname = 'mentions')
+	{
+		$path = e_PLUGIN . 'mentions/' . $logname . '.txt';
+		@file_put_contents($path, $content . "\n", FILE_APPEND);
+		unset($path, $content);
+	}
+
+
+	/**
+	 * Notify all mentionees in a post event
+	 */
+	protected function notifyAllMentioned()
+	{
+		$mentions = $this->mentions;
+
+		if (null === $mentions || $mentions !== (array)$mentions) {
+			return;
+		}
+
+		foreach ($mentions as $mention) {
+			$this->mentioneeData = $this->getUserData($mention);
+
+			// Debug
+			$this->log(json_encode($this->mentioneeData), 'mentionee-data');
+
+			// email
+			if ($this->email($this->mentioneeData)) {
+				unset($this->mentioneeData);
+			}
+
+		}
+
+	}
+
+
+	/**
+	 * Gets all mentions in the message
+	 *
+	 * @return array | null
+	 */
+	private function getAllMentions($message)
+	{
+		$pattern = '/(?<=\W|^)@([a-z0-9_.]*)/mis';
+
+		if (preg_match_all($pattern, $message, $matches) !== false) {
+			return $matches[0];
+		}
+
+		return null;
+	}
+
+
+	/**
+	 *    Send an email to notify of a mention
+	 *
+	 * @param array $userData - user data
+	 * @param array $contentData - Content details
+	 *
+	 * @return boolean
+	 */
+	protected function email($userData)
+	{
+		$MENTIONS_NOTIFY = $this->template();
+
+		$mentionee_name = $userData['user_name'];
+		$mentioner = $this->mentioner;
+		$content = $this->getContentType();
+		$date = e107::getParser()->toDate(time(), 'long');
+		$url = $this->getMentionContentLink();
+
+		$body = [
+			'USERNAME' => $mentionee_name,
+			'DATE'     => $date,
+			'SITENAME' => SITENAME,
+			'MENTIONER' => $mentioner,
+			'CONTENT_TYPE' => $content,
+			'URL'      => $url,
+		];
+
+		$text = e107::getParser()->simpleParse($MENTIONS_NOTIFY, $body);
+
+		$eml = [];
+		$eml['email_subject'] = SITENAME . ': You were mentioned by ' . $mentioner;
+		$eml['send_html'] = true;
+		$eml['email_body'] = $text;
+		$eml['template'] = 'default';
+		$eml['e107_header'] = $userData['user_id'];
+
+		if (e107::getEmail()
+			->sendEmail($userData['user_email'], $userData['user_name'], $eml)
+		) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+
+	/**
+	 * @return array|string
+	 */
+	protected function template()
+	{
+		$MENTIONS_NOTIFY = e107::getTemplate('mentions', 'mentions', 'notify');
+
+		if (empty($MENTIONS_NOTIFY))
+		{
+
+			$MENTIONS_NOTIFY = '<div>
+			<h4>You have been mentioned!</h4>
+			<p>Hello {USERNAME}</p>
+			<p>{MENTIONER} mentioned you in a {CONTENT_TYPE} at {SITENAME} on {DATE}.</p>
+			<p>Please follow the {URL} to view.</p>
+			</div>';
+
+		}
+
+		return $MENTIONS_NOTIFY;
+	}
+
+
+	/**
+	 * todo: develop this stub
+	 *
+	 * @return string
+	 */
+	protected function getMentionContentLink()
+	{
+		return '--LINK---';
+	}
+
+
+	/**
+	 * todo: develop this stub
+	 *
+	 * @return string
+	 */
+	private function getContentType()
+	{
+		return '--CONTENT_TYPE---';
+	}
+
+
+	public static function execute()
+	{
+		$mn = new MentionsNotification;
+		$mn->perform();
+	}
 
 }
