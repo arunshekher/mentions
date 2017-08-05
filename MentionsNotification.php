c5c39c5b826954e6866c9f0b4eabc03790d698a1
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-05 05:08:57 +0400

    Refactoring - Remove duplicates from mentions, Optimize email method.

--- MentionsNotification.php
+++ MentionsNotification.php
@@ -4,26 +4,38 @@
 class MentionsNotification extends Mentions
 {
 	protected $mentions;
-	protected $message;
-	protected $content;
-
 	protected $mentioner;
 	protected $mentioneeData;
+
+	protected $contentType;
+	protected $contentMessage;
+	protected $contentId;
+	protected $contentUrl;
+
+
+	/**
+	 * Public static method to call perform()
+	 */
+	public static function execute()
+	{
+		$mn = new MentionsNotification;
+		$mn->perform();
+	}
 
 
 	/**
 	 * Listen to and perform notification
 	 */
-	public function perform()
+	private function perform()
 	{
 		if (USER && (strtolower($_SERVER['REQUEST_METHOD']) === 'post' || e_AJAX_REQUEST)) {
-			// chatbox
+			// Chatbox
 			if ($_POST['chat_submit'] && $_POST['cmessage'] !== '') {
 				// Debug
 				$this->log(json_encode([USERNAME, $_POST['cmessage']]));
 				$mentions = $this->getAllMentions($_POST['cmessage']);
 				if ($mentions) {
-					$this->message = $_POST['cmessage'];
+					$this->contentMessage = $_POST['cmessage'];
 					$this->mentions = $mentions;
 					$this->mentioner = USERNAME;
 					$this->notifyAllMentioned();
@@ -36,41 +48,15 @@ class MentionsNotification extends Mentions
 
 	/**
 	 * Debug log method
+	 *
 	 * @param string $content
 	 * @param string $logname
 	 */
 	public function log($content, $logname = 'mentions')
 	{
 		$path = e_PLUGIN . 'mentions/' . $logname . '.txt';
-		@file_put_contents($path, $content . "\n", FILE_APPEND);
+		file_put_contents($path, $content . "\n", FILE_APPEND);
 		unset($path, $content);
-	}
-
-
-	/**
-	 * Notify all mentionees in a post event
-	 */
-	protected function notifyAllMentioned()
-	{
-		$mentions = $this->mentions;
-
-		if (null === $mentions || $mentions !== (array)$mentions) {
-			return;
-		}
-
-		foreach ($mentions as $mention) {
-			$this->mentioneeData = $this->getUserData($mention);
-
-			// Debug
-			$this->log(json_encode($this->mentioneeData), 'mentionee-data');
-
-			// email
-			if ($this->email($this->mentioneeData)) {
-				unset($this->mentioneeData);
-			}
-
-		}
-
 	}
 
 
@@ -92,7 +78,36 @@ class MentionsNotification extends Mentions
 
 
 	/**
-	 *    Send an email to notify of a mention
+	 * Notify all mentionees in a post event
+	 */
+	protected function notifyAllMentioned()
+	{
+		$mentions = $this->mentions;
+
+		if (null === $mentions || $mentions !== (array) $mentions) {
+			return;
+		}
+
+		//$mentions = array_unique($this->mentions, SORT_STRING);
+
+		foreach (array_unique($mentions, SORT_STRING) as $mention) {
+			$this->mentioneeData = $this->getUserData($mention);
+
+			// Debug
+			$this->log(json_encode($this->mentioneeData), 'mentionee-data');
+
+			// Email
+			if ($this->email($this->mentioneeData)) {
+				unset($this->mentioneeData);
+			}
+
+		}
+
+	}
+
+
+	/**
+	 * Send an email to notify of a mention
 	 *
 	 * @param array $userData - user data
 	 * @param array $contentData - Content details
@@ -101,6 +116,8 @@ class MentionsNotification extends Mentions
 	 */
 	protected function email($userData)
 	{
+		$mail = e107::getEmail();
+
 		$MENTIONS_NOTIFY = $this->template();
 
 		$mentionee_name = $userData['user_name'];
@@ -110,26 +127,27 @@ class MentionsNotification extends Mentions
 		$url = $this->getMentionContentLink();
 
 		$body = [
-			'USERNAME' => $mentionee_name,
-			'DATE'     => $date,
-			'SITENAME' => SITENAME,
-			'MENTIONER' => $mentioner,
+			'USERNAME'     => $mentionee_name,
+			'DATE'         => $date,
+			'SITENAME'     => SITENAME,
+			'MENTIONER'    => $mentioner,
 			'CONTENT_TYPE' => $content,
-			'URL'      => $url,
+			'URL'          => $url,
 		];
 
 		$text = e107::getParser()->simpleParse($MENTIONS_NOTIFY, $body);
 
 		$eml = [];
-		$eml['email_subject'] = SITENAME . ': You were mentioned by ' . $mentioner;
+		$eml['email_subject'] = 'You were mentioned by ' . $mentioner;
 		$eml['send_html'] = true;
 		$eml['email_body'] = $text;
 		$eml['template'] = 'default';
 		$eml['e107_header'] = $userData['user_id'];
 
-		if (e107::getEmail()
-			->sendEmail($userData['user_email'], $userData['user_name'], $eml)
-		) {
+		$send = $mail->sendEmail($userData['user_email'], $userData['user_name'], $eml);
+		unset($body, $text, $eml);
+
+		if ($send) {
 			return true;
 		} else {
 			return false;
@@ -145,8 +163,7 @@ class MentionsNotification extends Mentions
 	{
 		$MENTIONS_NOTIFY = e107::getTemplate('mentions', 'mentions', 'notify');
 
-		if (empty($MENTIONS_NOTIFY))
-		{
+		if (empty($MENTIONS_NOTIFY)) {
 
 			$MENTIONS_NOTIFY = '<div>
 			<h4>You have been mentioned!</h4>
@@ -166,9 +183,9 @@ class MentionsNotification extends Mentions
 	 *
 	 * @return string
 	 */
-	protected function getMentionContentLink()
+	private function getContentType()
 	{
-		return '--LINK---';
+		return '--CONTENT_TYPE---';
 	}
 
 
@@ -177,16 +194,9 @@ class MentionsNotification extends Mentions
 	 *
 	 * @return string
 	 */
-	private function getContentType()
+	protected function getMentionContentLink()
 	{
-		return '--CONTENT_TYPE---';
-	}
-
-
-	public static function execute()
-	{
-		$mn = new MentionsNotification;
-		$mn->perform();
+		return '--LINK---';
 	}
 
 }
