fc0320ea06f909553459f07556b44e29acb35b36
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-05 06:57:10 +0400

    Move notification execution to e_module.php

--- e_header.php
+++ e_header.php
@@ -2,12 +2,12 @@
 if ( ! defined('e107_INIT')) {
 	exit;
 }
-require __DIR__ . '/MentionsNotification.php';
+//require __DIR__ . '/MentionsNotification.php';
 require __DIR__ . '/MentionsSuggestion.php';
 
 // load suggestion popup libs
 MentionsSuggestion::libs();
 
 // perform mentions notification
-MentionsNotification::execute();
+//MentionsNotification::execute();
 
---
+++ e_module.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/MentionsNotification.php';
+// perform mentions notification
+MentionsNotification::execute();
