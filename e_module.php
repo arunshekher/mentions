9f7029cb2ad01cc5e323872fac4cd0bd1c9cb883
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-05 10:22:35 +0400

    Code cleanup.

--- e_module.php
+++ e_module.php
@@ -1,10 +1,5 @@
 <?php
 require_once __DIR__ . '/MentionsNotification.php';
 
-// chatbox notification
+// execute notification
 MentionsNotification::execute();
-
-// For the rest event trigger works
-//e107::getEvent()->register('user_comment_posted', array('MentionsNotification', 'commentsMentionsNotify'));
-//e107::getEvent()->register('user_forum_topic_created', array('MentionsNotification', 'forumsMentionsNotify'));
-//e107::getEvent()->register('user_forum_post_created', array('MentionsNotification', 'forumsMentionsNotify'));
