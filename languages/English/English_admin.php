3bbaf3156bc01a1e7d3b5bbf3a80317e162e180c
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-08-07 16:24:03 +0400

    Add language defines for newly created preferences.

--- languages/English/English_admin.php
+++ languages/English/English_admin.php
@@ -8,6 +8,11 @@ define('LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS', '<p>Min. number of characters to 
 define('LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS', '<p>Max number of char. after <kbd>@</kbd> that would be matched to populate suggestion</p>  <kbd>Upto: 20</kbd>');
 define('LAN_MENTIONS_PREF_LBL_ATWHO_LIMIT', '<p>Number of username entries to show in popup-list</p><kbd>Recommended: 5</kbd>');
 define('LAN_MENTIONS_PREF_LBL_ATWHO_HIGHLIGHT', 'Highlight first entry in popup-list:');
+define('LAN_MENTIONS_PREF_LBL_CHATBOX_EMAIL', 'Email notification for chatbox mentions');
+define('LAN_MENTIONS_PREF_LBL_COMMENT_EMAIL', 'Email notification for comment mentions');
+define('LAN_MENTIONS_PREF_LBL_FORUMTOPIC_EMAIL', 'Email notification for forum new topic mentions');
+define('LAN_MENTIONS_PREF_LBL_FORUMREPLY_EMAIL', 'Email notification for forum reply mentions');
+define('LAN_MENTIONS_PREF_LBL_EMAIL_SUBJECT', 'Email Subject Line');
 
 define('LAN_MENTIONS_PREF_LBL_HINT_ACTIVATION', 'Turn On/Off Mentions Globally');
 define('LAN_MENTIONS_PREF_LBL_HINT_CONTEXT', '\'mentions\' is called in what text parse context.');
@@ -16,6 +21,12 @@ define('LAN_MENTIONS_PREF_LBL_HINT_ATWHO_MINCHAR', 'Minimum number of characters
 define('LAN_MENTIONS_PREF_LBL_HINT_ATWHO_MAXCHAR', 'Max number of characters after `@` that would be matched to populate suggestion.');
 define('LAN_MENTIONS_PREF_LBL_HINT_ATWHO_LIMIT', 'Number of username entries to show in suggestion popup-list');
 define('LAN_MENTIONS_PREF_LBL_HINT_ATWHO_HIGHLIGHT', 'Toggle highlight on/off for the first entry in popup-list.');
+define('LAN_MENTIONS_PREF_LBL_HINT_CHATBOX_EMAIL', 'Turn on email notification for chatbox mentions.');
+define('LAN_MENTIONS_PREF_LBL_HINT_COMMENT_EMAIL', 'Turn on email notification for comment mentions.');
+define('LAN_MENTIONS_PREF_LBL_HINT_FORUMTOPIC_EMAIL', 'Turn on email notification for new forum topic mentions.');
+define('LAN_MENTIONS_PREF_LBL_HINT_FORUMREPLY_EMAIL', 'Turn on email notification for forum reply mentions.');
+define('LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT', 'Email Subject Line - {MENTIONER} will be substituted with actual mentioner\'s username at run-time.');
+
 
 define('LAN_MENTIONS_PREF_VAL_CONTEXT_01', 'Forum + Chatbox');
 define('LAN_MENTIONS_PREF_VAL_CONTEXT_02', 'Forum + Chatbox + Comments');
@@ -30,7 +41,7 @@ define('LAN_MENTIONS_INFO_MENU_SUBTITLE_DEV', '<h5>Developer:</h5>');
 define('LAN_MENTIONS_INFO_MENU_DEV', '<p><small>Arun S. Sekher</small></p>');
 define('LAN_MENTIONS_INFO_MENU_GITHUB_BUTTONS_SCRIPT', '<script async defer src="https://buttons.github.io/buttons.js"></script>');
 define('LAN_MENTIONS_INFO_MENU_REPO_BUTTON_FOLLOW', '<a class="github-button" href="https://github.com/arunshekher" aria-label="Follow @arunshekher on GitHub">Follow</a>');
-define('LAN_MENTIONS_INFO_MENU_REPO_URL', '<p><kbd style="word-wrap: break-word"><a href="http://github.com/arunshekher/mentions">http://github.com/arunshekher/mentions</a></kbd></p>');
+define('LAN_MENTIONS_INFO_MENU_REPO_URL', '<p><kbd style="word-wrap: break-word"><a href="http://github.com/arunshekher/mentions" target="_blank">http://github.com/arunshekher/mentions</a></kbd></p>');
 define('LAN_MENTIONS_INFO_MENU_REPO_BUTTON_WATCH', '<a class="github-button" href="https://github.com/arunshekher/mentions/subscription" data-icon="octicon-eye" aria-label="Watch arunshekher/mentions on GitHub">Watch</a>');
 define('LAN_MENTIONS_INFO_MENU_REPO_BUTTON_STAR', ' <a class="github-button" href="https://github.com/arunshekher/mentions" data-icon="octicon-star" aria-label="Star arunshekher/mentions on GitHub">Star</a>');
 define('LAN_MENTIONS_INFO_MENU_REPO_BUTTON_ISSUE', ' <a class="github-button" href="https://github.com/arunshekher/mentions/issues" data-icon="octicon-issue-opened" aria-label="Issue arunshekher/mentions on GitHub">Issue</a>');
