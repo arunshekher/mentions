760f24e93849e58c88f1a7f664f652b5b80e24c2
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-09-08 20:39:26 +0400

    Add English_front language file.

---
+++ languages/English/English_front.php
@@ -0,0 +1,9 @@
+<?php
+define('LAN_MENTIONS_CONTENT_CHATBOX', 'chatbox post');
+define('LAN_MENTIONS_CONTENT_COMMENT', 'comment post');
+define('LAN_MENTIONS_CONTENT_FORUM_TOPIC', 'new forum topic');
+define('LAN_MENTIONS_CONTENT_FORUM_REPLY', 'forum reply');
+
+// $this->mentioner mentioned you in a $this->itemTag on $this->mentionDate.
+
+define('LAN_MENTIONS_EMAIL_VERSE_CHATBOX', '[x] mentioned you in a [y] on [z].');
