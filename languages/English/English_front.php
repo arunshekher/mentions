26b9c4c767ee798945f84e5f33a3a74b45eb8dbf
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-09-09 08:20:54 +0400

    Add new LAN constants for mention verse.

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -4,6 +4,7 @@ define('LAN_MENTIONS_CONTENT_COMMENT', 'comment post');
 define('LAN_MENTIONS_CONTENT_FORUM_TOPIC', 'new forum topic');
 define('LAN_MENTIONS_CONTENT_FORUM_REPLY', 'forum reply');
 
-// $this->mentioner mentioned you in a $this->itemTag on $this->mentionDate.
-
-define('LAN_MENTIONS_EMAIL_VERSE_CHATBOX', '[x] mentioned you in a [y] on [z].');
+define('LAN_MENTIONS_EMAIL_VERSE_CHATBOX', '[a] mentioned you in a [b] on [c].');
+define('LAN_MENTIONS_EMAIL_VERSE_COMMENT', '[a] mentioned you in a [b] for the [d] item titled \'[e]\' on [c].');
+define('LAN_MENTIONS_EMAIL_VERSE_FORUM', '[a] mentioned you in a [b] on [c].');
+define('LAN_MENTIONS_EMAIL_VERSE_UNRESOLVED', '[a] mentioned you in an un-resolvable post!');
