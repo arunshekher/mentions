9dcd2b8bfe1b2f4393de7c40c97ec2ace8098486
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-09-09 10:49:28 +0400

    Modify mention verse LANS.

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -4,7 +4,7 @@ define('LAN_MENTIONS_CONTENT_COMMENT', 'comment post');
 define('LAN_MENTIONS_CONTENT_FORUM_TOPIC', 'new forum topic');
 define('LAN_MENTIONS_CONTENT_FORUM_REPLY', 'forum reply');
 
-define('LAN_MENTIONS_EMAIL_VERSE_CHATBOX', '[a] mentioned you in a [b] on [c].');
-define('LAN_MENTIONS_EMAIL_VERSE_COMMENT', '[a] mentioned you in a [b] for the [d] item titled \'[e]\' on [c].');
-define('LAN_MENTIONS_EMAIL_VERSE_FORUM', '[a] mentioned you in a [b] on [c].');
-define('LAN_MENTIONS_EMAIL_VERSE_UNRESOLVED', '[a] mentioned you in an un-resolvable post!');
+define('LAN_MENTIONS_EMAIL_VERSE_CHATBOX', '[user] mentioned you in a [tag] on [date].');
+define('LAN_MENTIONS_EMAIL_VERSE_COMMENT', '[user] mentioned you in a [tag] for the [type] item titled \'[title]\' on [date].');
+define('LAN_MENTIONS_EMAIL_VERSE_FORUM', '[user] mentioned you in a [tag] on [date].');
+define('LAN_MENTIONS_EMAIL_VERSE_UNRESOLVED', '[x] mentioned you in an un-resolvable post!');
