d35c2c54bd8c39048b8936e6b151fff9c4d6c6c3
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-09-09 16:23:10 +0400

    Add LAN constant for email 'Hello'

--- languages/English/English_front.php
+++ languages/English/English_front.php
@@ -3,6 +3,8 @@ define('LAN_MENTIONS_TAG_CHATBOX', 'chatbox');
 define('LAN_MENTIONS_TAG_COMMENT', 'comment');
 define('LAN_MENTIONS_TAG_FORUM', 'forum');
 
+define('LAN_MENTIONS_EMAIL_HELLO', 'Hello');
+
 define('LAN_MENTIONS_EMAIL_VERSE_CHATBOX', '[user] mentioned you in a [tag] post on [date].');
 define('LAN_MENTIONS_EMAIL_VERSE_COMMENT', '[user] mentioned you in a [tag] post for the [type] item titled \'[title]\' on [date].');
 define('LAN_MENTIONS_EMAIL_VERSE_FORUM', '[user] mentioned you in a [tag] post on [date].');
