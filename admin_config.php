3e00a8b63e4466146a2413c36d74682586dbfc58
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-09-16 14:05:33 +0400

    Remove html from LAN terms - stick to e107 LAN standard.

--- admin_config.php
+++ admin_config.php
@@ -7,6 +7,7 @@ if ( ! getperms('P') || ! e107::isInstalled('mentions')) {
 
 e107::lan('mentions', 'admin', true);
 
+
 class mentions_adminArea extends e_admin_dispatcher
 {
 	protected $modes = [
@@ -49,88 +50,108 @@ class mentions_ui extends e_admin_ui
 	];
 
 	protected $prefs = [
-		'mentions_active'             => [
-			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_ACTIVE
-				. '</p><small>' . LAN_MENTIONS_PREF_LBL_HINT_ACTIVATION . '</small>',
+		'mentions_active'         => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_ACTIVE . '</p><small>'
+				. LAN_MENTIONS_PREF_LBL_HINT_ACTIVATION . '</small>',
 			'tab'   => 0,
 			'type'  => 'boolean',
 			'data'  => 'int',
 			'help'  => LAN_MENTIONS_PREF_LBL_HINT_ACTIVATION,
 		],
-		'mentions_contexts'           => [
-			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_CONTEXTS
-				. '</p><small>' . LAN_MENTIONS_PREF_LBL_HINT_CONTEXT . '</small>',
+		'mentions_contexts'       => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_CONTEXTS . '</p><small>'
+				. LAN_MENTIONS_PREF_LBL_HINT_CONTEXT . '</small>',
 			'tab'   => 0,
 			'type'  => 'dropdown',
 			'size'  => 'xxxlarge',
 			'data'  => 'int',
 			'help'  => LAN_MENTIONS_PREF_LBL_HINT_CONTEXT,
 		],
-		'use_global_path'             => [
-			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_GLOBAL_LIBS
-				. '</p><small>' . LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS . '</small>',
+		'use_global_path'         => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_GLOBAL_LIBS . '</p><small>'
+				. LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS_1
+				. '<kbd><a href="https://github.com/ichord/Caret.js" target="_blank">Caret.js</a></kbd>'
+				. LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS_2
+				. '<kbd><a href="https://github.com/ichord/At.js" target="_blank">At.js</a></kbd>'
+				. LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS_3 . '</small>',
 			'tab'   => 0,
 			'type'  => 'boolean',
 			'data'  => 'int',
-			'help'  => LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS,
+			'help'  => LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS_1
+				. '<kbd><a href="https://github.com/ichord/Caret.js" target="_blank">Caret.js</a></kbd>'
+				. LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS_2
+				. '<kbd><a href="https://github.com/ichord/At.js" target="_blank">At.js</a></kbd>'
+				. LAN_MENTIONS_PREF_LBL_HINT_GLOBAL_LIBS_3,
 		],
-		'atwho_min_char'              => [
-			'title' => LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS,
+		'atwho_min_char'          => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS_1 . '<kbd>@</kbd>'
+				. LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS_2 . '</p><kbd>'
+				. LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS_3 . '</kbd>',
 			'tab'   => 1,
 			'type'  => 'number',
 			'data'  => 'int',
-			'help'  => LAN_MENTIONS_PREF_LBL_HINT_ATWHO_MINCHAR,
+			'help'  => '<p>' . LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS_1 . '<kbd>@</kbd>'
+				. LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS_2 . '</p><kbd>'
+				. LAN_MENTIONS_PREF_LBL_ATWHO_MINCHARS_3 . '</kbd>',
 		],
-		'atwho_max_char'              => [
-			'title' => LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS,
+		'atwho_max_char'          => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS_1 . '<kbd>@</kbd>'
+				. LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS_2
+				. '</p><kbd>' . LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS_3 . '</kbd>',
 			'tab'   => 1,
 			'type'  => 'number',
 			'data'  => 'int',
-			'help'  => LAN_MENTIONS_PREF_LBL_HINT_ATWHO_MAXCHAR,
+			'help'  => '<p>' . LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS_1 . '<kbd>@</kbd>'
+				. LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS_2
+				. '</p><kbd>' . LAN_MENTIONS_PREF_LBL_ATWHO_MAXCHARS_3 . '</kbd>',
 		],
-		'atwho_item_limit'            => [
-			'title' => LAN_MENTIONS_PREF_LBL_ATWHO_LIMIT,
+		'atwho_item_limit'        => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_ATWHO_LIMIT_1
+				. '</p><kbd>' . LAN_MENTIONS_PREF_LBL_ATWHO_LIMIT_2 . '</kbd>',
 			'tab'   => 1,
 			'type'  => 'number',
 			'data'  => 'int',
-			'help'  => LAN_MENTIONS_PREF_LBL_HINT_ATWHO_LIMIT,
+			'help'  => '<p>' . LAN_MENTIONS_PREF_LBL_ATWHO_LIMIT_1
+				. '</p><kbd>' . LAN_MENTIONS_PREF_LBL_ATWHO_LIMIT_2 . '</kbd>',
 		],
-		'atwho_highlight_first'       => [
+		'atwho_highlight_first'   => [
 			'title' => LAN_MENTIONS_PREF_LBL_ATWHO_HIGHLIGHT,
 			'tab'   => 1,
 			'type'  => 'boolean',
 			'data'  => 'int',
 			'help'  => LAN_MENTIONS_PREF_LBL_HINT_ATWHO_HIGHLIGHT,
 		],
-		'notify_chatbox_mentions'     => [
+		'notify_chatbox_mentions' => [
 			'title' => LAN_MENTIONS_PREF_LBL_CHATBOX_EMAIL,
 			'tab'   => 2,
 			'type'  => 'boolean',
 			'data'  => 'int',
 			'help'  => LAN_MENTIONS_PREF_LBL_HINT_CHATBOX_EMAIL,
 		],
-		'notify_comment_mentions'     => [
+		'notify_comment_mentions' => [
 			'title' => LAN_MENTIONS_PREF_LBL_COMMENT_EMAIL,
 			'tab'   => 2,
 			'type'  => 'boolean',
 			'data'  => 'int',
 			'help'  => LAN_MENTIONS_PREF_LBL_HINT_COMMENT_EMAIL,
 		],
-		'notify_forum_mentions' => [
+		'notify_forum_mentions'   => [
 			'title' => LAN_MENTIONS_PREF_LBL_FORUM_EMAIL,
 			'tab'   => 2,
 			'type'  => 'boolean',
 			'data'  => 'int',
 			'help'  => LAN_MENTIONS_PREF_LBL_HINT_FORUM_EMAIL,
 		],
-		'email_subject_line'          => [
-			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_EMAIL_SUBJECT
-				. '</p><small>' . LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT . '</small>',
+		'email_subject_line'      => [
+			'title' => '<p>' . LAN_MENTIONS_PREF_LBL_EMAIL_SUBJECT . '</p><small>'
+				. LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT_1 . '<kbd>{MENTIONER}</kbd>'
+				. LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT_2 . '</small>',
 			'tab'   => 2,
 			'type'  => 'text',
 			'data'  => 'str',
 			'size'  => 'xxlarge',
-			'help'  => LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT,
+			'help'  => LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT_1 . '<kbd>{MENTIONER}</kbd>'
+				. LAN_MENTIONS_PREF_LBL_HINT_EMAIL_SUBJECT_2,
 		],
 
 	];
@@ -149,16 +170,16 @@ class mentions_ui extends e_admin_ui
 	{
 		$caption = LAN_MENTIONS_INFO_MENU_TITLE;
 		$text = '';
-		$text .= LAN_MENTIONS_INFO_MENU_LOGO;
-		$text .= LAN_MENTIONS_INFO_MENU_SUBTITLE;
-		$text .= LAN_MENTIONS_INFO_MENU_REPO_URL;
-		$text .= LAN_MENTIONS_INFO_MENU_REPO_BUTTON_WATCH;
-		$text .= LAN_MENTIONS_INFO_MENU_REPO_BUTTON_STAR;
-		$text .= LAN_MENTIONS_INFO_MENU_REPO_BUTTON_ISSUE;
-		$text .= LAN_MENTIONS_INFO_MENU_SUBTITLE_DEV;
-		$text .= LAN_MENTIONS_INFO_MENU_DEV;
-		$text .= LAN_MENTIONS_INFO_MENU_REPO_BUTTON_FOLLOW;
-		$text .= LAN_MENTIONS_INFO_MENU_GITHUB_BUTTONS_SCRIPT;
+		$text .= '<div style="text-align: center"><img src="http://www.e107.space/projects/mentions/svg" alt="Mentions" width="128" height="128"></div>';
+		$text .= '<br><h5>' . LAN_MENTIONS_INFO_MENU_SUBTITLE . '</h5>';
+		$text .= '<p><kbd style="word-wrap: break-word"><a href="http://github.com/arunshekher/mentions" target="_blank">http://github.com/arunshekher/mentions</a></kbd></p>';
+		$text .= '<a class="github-button" href="https://github.com/arunshekher/mentions/subscription" data-icon="octicon-eye" aria-label="Watch arunshekher/mentions on GitHub">Watch</a>';
+		$text .= ' <a class="github-button" href="https://github.com/arunshekher/mentions" data-icon="octicon-star" aria-label="Star arunshekher/mentions on GitHub">Star</a>';
+		$text .= ' <a class="github-button" href="https://github.com/arunshekher/mentions/issues" data-icon="octicon-issue-opened" aria-label="Issue arunshekher/mentions on GitHub">Issue</a>';
+		$text .= '<h5>' . LAN_MENTIONS_INFO_MENU_SUBTITLE_DEV . '</h5>';
+		$text .= '<p><small>Arun S. Sekher</small></p>';
+		$text .= '<a class="github-button" href="https://github.com/arunshekher" aria-label="Follow @arunshekher on GitHub">Follow</a>';
+		$text .= '<script async defer src="https://buttons.github.io/buttons.js"></script>';
 
 		return ['caption' => $caption, 'text' => $text];
 
