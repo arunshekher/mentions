21625ba15c0b3abfd5d089882fdf1d576a720509
Arun S. Sekher <arunshekher@users.noreply.github.com>
2019-01-26 19:42:15 +0400

    Add experimental 'content links' and 'content emails' implementation using  a factory design pattern - all required classes declared in a single file.
    Signed-off-by: Arun S. Sekher <arunshekher@users.noreply.github.com>

---
+++ MentionsContentLinks.php
@@ -0,0 +1,149 @@
+<?php
+
+class ContentLinksFactory
+{
+	private $id;
+	private $data;
+
+
+	/**
+	 * ContentLinksFactory constructor.
+	 *
+	 * @param $id
+	 * @param $data
+	 */
+	public function __construct($id, $data)
+	{
+		$this->id = $id;
+		$this->data = $data;
+	}
+
+
+	public function create()
+	{
+
+	}
+}
+
+class CommentLinks
+{
+
+}
+
+class ForumLinks
+{
+
+	private $forumData;
+
+
+	/**
+	 * ForumLinks constructor.
+	 *
+	 * @param $forumData
+	 */
+	public function __construct($forumData)
+	{
+		$this->setForumData($forumData)->setMissingForumData();
+	}
+
+
+	/**
+	 * @param mixed $forumData
+	 *
+	 * @return ForumLinks
+	 */
+	public function setForumData($forumData)
+	{
+		$this->forumData = $forumData;
+
+		return $this;
+	}
+
+
+	/**
+	 * @return $this
+	 */
+	private function setMissingForumData()
+	{
+		if (is_array($this->forumData)) {
+			array_merge($this->forumData, $this->getMissingForumData());
+			// todo: create thread_sef here?
+		}
+		return $this;
+	}
+
+
+
+
+	private function createSlug($title, $type = null)
+	{
+		$type = $type ?: e107::getPref('url_sef_translate');
+
+		return eHelper::title2sef($title, $type);
+	}
+
+
+	private function createForumItemLink()
+	{
+
+		$data = $this->forumData;
+
+		$data['thread_sef'] = $this->getThreadSlug();
+
+		$opt = $this->getLinkOptions();
+
+		return e107::url('forum', 'topic', $data, $opt);
+	}
+
+
+	/**
+	 * Returns missing forum data from `#forum` and `#forum_thread` tables
+	 *
+	 * @return array|bool|null
+	 */
+	private function getMissingForumData()
+	{
+		$sql = e107::getDb();
+
+		$thread_id = (int) $this->forumData['post_thread'];
+
+		$query = "SELECT f.forum_sef, f.forum_id, ft.thread_id, ft.thread_name 
+					FROM `#forum` AS f 
+						LEFT JOIN `#forum_thread` AS ft ON f.forum_id = ft.thread_forum_id 
+							WHERE ft.thread_id = {$thread_id} ";
+
+		$result = $sql->gen($query);
+
+		if ($result) {
+			return $sql->fetch($result);
+		}
+
+		return null;
+	}
+
+
+	private function getThreadSlug()
+	{
+
+		return $this->createSlug($this->forumData['thread_name']);
+
+	}
+
+
+	/**
+	 * Returns forum link options
+	 *
+	 * @return array
+	 */
+	private function getLinkOptions()
+	{
+		$urlPref = e107::getPref('e_url_list');
+
+		if ($urlPref['forum']) {
+			return ['mode'   => 'full', 'legacy' => false, 'query'  => ['last' => 1]];
+		}
+		return ['mode' => 'full', 'legacy' => true];
+	}
+
+
+}
